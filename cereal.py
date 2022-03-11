4cde3082e2e05ab82f6f84e721130ae71b965246
christy gabbard <christygabbard82@gmail.com>
2022-03-11 13:28:45 -0500

    updated

--- cereal.py
+++ cereal.py
@@ -4,15 +4,18 @@ import pandas as pd
 pd.options.display.max_rows = 9999
 pd.options.display.max_columns = 9999
 df = pd.read_csv('cereal.csv')
-df = dict(zip(df.name, df.sugars))
+df = dict(zip(df.name, df.sugars, df.fat))
+df = fat
 finished = False
 while finished == False: #while finished = false, the while loop will continue to run
     cereal1 = input("Enter a cereal: ")
     cereal2 = input("Enter a cereal: ")
     cereal1_sugar = df.get(cereal1)
     cereal2_sugar = df.get(cereal2)
+    cereal1_fats = df.get(cereal1)
+    cereal2_fats = df.get(cereal2)
 
-    print(f"{cereal1} has {cereal1_sugar} grams of sugar. {cereal2} has {cereal2_sugar} grams of sugar.")
+    print(f"{cereal1} has {cereal1_sugar} grams of sugar and {cereal1_fats} grams of fat {cereal2} has {cereal2_sugar} grams of sugar and {cereal2_fats} grams of fat")
     if cereal1_sugar > cereal2_sugar:
         print(f"{cereal2} has less sugar than {cereal1}")
     if cereal1_sugar < cereal2_sugar:
@@ -20,7 +23,7 @@ while finished == False: #while finished = false, the while loop will continue t
     if cereal1_sugar == cereal2_sugar:
         print(f"{cereal2} has same amount as {cereal1}")
     x = np.array([cereal1, cereal2])
-    y = np.array([cereal1_sugar, cereal2_sugar])
+    y = np.array([cereal1_sugar, cereal2_sugar, cereal1_fats, cereal2_fats])
 
     plt.bar(x,y)
     plt.show()
